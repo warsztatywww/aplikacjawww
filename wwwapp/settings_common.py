ccafac3a41d3af03abd2ef6e21b27786ea316933
Artur Puzio <contact@puzio.waw.pl>
2020-07-16 20:16:27 +0200

    Security enchancments
    - Enabled Django SecurityMiddleware
    - SESSION_COOKIE_SECURE
    - CSRF_COOKIE_SECURE
    - Deny cross frame

--- wwwapp/settings_common.py
+++ wwwapp/settings_common.py
@@ -44,6 +44,7 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'wwwapp.urls'
