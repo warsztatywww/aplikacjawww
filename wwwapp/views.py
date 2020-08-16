06637730e9fea97ff04994ff7dd2730d3b418a53
krzys-h <krzys_h@interia.pl>
2020-08-16 12:34:35 +0200

    Fix admins getting automatically added to workshop lecturers when editing

--- wwwapp/views.py
+++ wwwapp/views.py
@@ -234,9 +234,10 @@ def workshop_view(request, name=None):
                 workshop = form.save(commit=False)
                 workshop.save()
                 form.save_m2m()
-                user_profile = UserProfile.objects.get(user=request.user)
-                workshop.lecturer.add(user_profile)
-                workshop.save()
+                if new:
+                    user_profile = UserProfile.objects.get(user=request.user)
+                    workshop.lecturer.add(user_profile)
+                    workshop.save()
                 messages.info(request, 'Zapisano.')
                 return redirect('workshop', form.instance.name)
         else:
