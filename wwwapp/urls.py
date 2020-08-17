c75f2c56a111cfec5841f3e17e8db4de133ec8a4
krzys-h <krzys_h@interia.pl>
2020-08-17 22:46:45 +0200

    Upgrade urls.py to modern Django syntax with path()

--- wwwapp/urls.py
+++ wwwapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import logout_then_login
@@ -10,54 +10,55 @@ from . import views, mail_views
 from .auth import login_view, finish_merge_verification
 
 urlpatterns = [
-    url(
-        r'^favicon.ico$',
+    path(
+        'favicon.ico',
         RedirectView.as_view(
             url=staticfiles_storage.url('images/favicon.ico'),
             permanent=False),
         name="favicon"
     ),
-    url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^logout/$', logout_then_login, name='logout'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^gallery/', include('gallery.urls')),
-    url(r'^login/$', login_view, name='login'),
-    url(r'^accounts/', include('social_django.urls', namespace='social')),
-    url(r'^accounts/verified/$', finish_merge_verification, name='finish_merge_verification'),
-    url(r'^profile/(?P<user_id>[0-9]+)/$', views.profile_view, name='profile'),
-    url(r'^profile/$', views.my_profile_edit_view, name='edit_my_profile'),
-    url(r'^article/(?P<name>[a-zA-Z0-9\-_]+)/$', views.article_view, name='article'),
-    url(r'^article/(?P<name>[a-zA-Z0-9\-_]+)/edit/$', views.article_edit_view, name='article_edit'),
-    url(r'^addArticle/$', views.article_edit_view, name='article_add'),
-    url(r'^upload/(?P<type>article|workshop)/(?P<name>[a-zA-Z0-9\-_]+)/$', views.upload_file, name='upload'),
-    url(r'^articleNameList/$', views.article_name_list_view, name='articleNameList'),
-    url(r'^workshop/(?P<name>[a-zA-Z0-9\-_]+)/$', views.workshop_page_view, name='workshop_page'),
-    url(r'^workshop/(?P<name>[a-zA-Z0-9\-_]+)/priv/$', views.workshop_proposal_view, name='workshop_proposal'),
-    url(r'^workshop/(?P<name>[a-zA-Z0-9\-_]+)/edit/$', views.workshop_page_edit_view, name='workshop_page_edit'),
-    url(r'^workshop/(?P<name>[a-zA-Z0-9\-_]+)/participants/$', views.workshop_participants_view, name='workshop_participants'),
-    url(r'^savePoints/$', views.save_points_view),
-    url(r'^register/$', views.register_to_workshop_view, name='register_to_workshop'),
-    url(r'^unregister/$', views.unregister_from_workshop_view, name='unregister_from_workshop'),
-    url(r'^qualProblems/(?P<workshop_name>[a-zA-Z0-9\-_]+)/$', views.qualification_problems_view, name='qualification_problems'),
-    url(r'^addWorkshop/$', views.workshop_proposal_view, name='addWorkshop'),
-    url(r'^yourWorkshops/$', views.your_workshops_view, name='yourWorkshops'),
-    url(r'^allWorkshops/$', views.all_workshops_view, name='allWorkshops'),
-    url(r'^dataForPlan/$', RedirectView.as_view(url='/%d/dataForPlan/' % settings.CURRENT_YEAR, permanent=False), name='dataForPlan'),
-    url(r'^([0-9]+)/dataForPlan/$', views.data_for_plan_view, name='year_dataForPlan'),
-    url(r'^participants/$', RedirectView.as_view(url='/%d/participants/' % settings.CURRENT_YEAR, permanent=False), name='participants'),
-    url(r'^([0-9]+)/participants/$', views.participants_view, name='year_participants'),
-    url(r'^lecturers/$', RedirectView.as_view(url='/%d/lecturers/' % settings.CURRENT_YEAR, permanent=False), name='lecturers'),
-    url(r'^([0-9]+)/lecturers/$', views.lecturers_view, name='year_lecturers'),
-    url(r'^people/$', views.participants_view, name='all_people'),
-    url(r'^emails/$', views.emails_view, name='emails'),
-    url(r'^filterEmails/(?:(?P<year>[0-9]+)/(?P<filter_id>[a-zA-Z0-9\-_]+)/)?$', mail_views.filtered_emails_view, name='filter_emails'),
-    url(r'^template_for_workshop_page/$', views.template_for_workshop_page_view, name='template_for_workshop_page'),
-    url(r'^program/$', RedirectView.as_view(url='/%d/program/' % settings.CURRENT_YEAR, permanent=False, query_string=True), name='program'),
-    url(r'^([0-9]+)/program/$', views.program_view, name='year_program'),
-    url(r'^resource_auth/$', views.resource_auth_view, name='resource_auth'),
-    url(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    url(r'^cloud/$', views.cloud_access_view, name='cloud_access'),
-    url(r'^$', views.index_view, name='index'),
+    path('tinymce/', include('tinymce.urls')),
+    path('logout/', logout_then_login, name='logout'),
+    path('admin/', admin.site.urls),
+    path('gallery/', include('gallery.urls')),
+    path('login/', login_view, name='login'),
+    path('accounts/', include('social_django.urls', namespace='social')),
+    path('accounts/verified/', finish_merge_verification, name='finish_merge_verification'),
+    path('profile/<int:user_id>/', views.profile_view, name='profile'),
+    path('profile/', views.my_profile_edit_view, name='edit_my_profile'),
+    path('article/<slug:name>/', views.article_view, name='article'),
+    path('article/<slug:name>/edit/', views.article_edit_view, name='article_edit'),
+    path('addArticle/', views.article_edit_view, name='article_add'),
+    path('upload/<slug:type>/<slug:name>/', views.upload_file, name='upload'),
+    path('articleNameList/', views.article_name_list_view, name='articleNameList'),
+    path('workshop/<slug:name>/', views.workshop_page_view, name='workshop_page'),
+    path('workshop/<slug:name>/priv/', views.workshop_proposal_view, name='workshop_proposal'),
+    path('workshop/<slug:name>/edit/', views.workshop_page_edit_view, name='workshop_page_edit'),
+    path('workshop/<slug:name>/participants/', views.workshop_participants_view, name='workshop_participants'),
+    path('savePoints/', views.save_points_view),
+    path('register/', views.register_to_workshop_view, name='register_to_workshop'),
+    path('unregister/', views.unregister_from_workshop_view, name='unregister_from_workshop'),
+    path('qualProblems/<slug:workshop_name>/', views.qualification_problems_view, name='qualification_problems'),
+    path('addWorkshop/', views.workshop_proposal_view, name='addWorkshop'),
+    path('yourWorkshops/', views.your_workshops_view, name='yourWorkshops'),
+    path('allWorkshops/', views.all_workshops_view, name='allWorkshops'),
+    path('dataForPlan/', RedirectView.as_view(url='/%d/dataForPlan/' % settings.CURRENT_YEAR, permanent=False), name='dataForPlan'),
+    path('([0-9]+)/dataForPlan/', views.data_for_plan_view, name='year_dataForPlan'),
+    path('participants/', RedirectView.as_view(url='/%d/participants/' % settings.CURRENT_YEAR, permanent=False), name='participants'),
+    path('([0-9]+)/participants/', views.participants_view, name='year_participants'),
+    path('lecturers/', RedirectView.as_view(url='/%d/lecturers/' % settings.CURRENT_YEAR, permanent=False), name='lecturers'),
+    path('([0-9]+)/lecturers/', views.lecturers_view, name='year_lecturers'),
+    path('people/', views.participants_view, name='all_people'),
+    path('emails/', views.emails_view, name='emails'),
+    path('filterEmails/', mail_views.filtered_emails_view, name='filter_emails'),
+    path('filterEmails/<int:year>/<int:filter_id>/', mail_views.filtered_emails_view, name='filter_emails'),
+    path('template_for_workshop_page/', views.template_for_workshop_page_view, name='template_for_workshop_page'),
+    path('program/', RedirectView.as_view(url='/%d/program/' % settings.CURRENT_YEAR, permanent=False, query_string=True), name='program'),
+    path('<int:year>/program/', views.program_view, name='year_program'),
+    path('resource_auth/', views.resource_auth_view, name='resource_auth'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('cloud/', views.cloud_access_view, name='cloud_access'),
+    path('', views.index_view, name='index'),
 ]
 
 if settings.DEBUG:
@@ -66,5 +67,5 @@ if settings.DEBUG:
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
