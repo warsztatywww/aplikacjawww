0b6a9ea19f3c1958acf0f5cf61e694b58f926310
krzys-h <krzys_h@interia.pl>
2020-12-29 14:03:31 +0100

    Setup coverage analysis

--- wwwapp/settings_debug.py
+++ wwwapp/settings_debug.py
@@ -3,6 +3,7 @@ from .settings_common import *
 SECRET_KEY = ')_7av^!cy(wfx=k#3*7x+(=j^fzv+ot^1@sh9s9t=8$bu@r(z$'
 
 DEBUG = True
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
