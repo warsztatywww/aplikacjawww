5e98d5c8a9eec4f41d37da21ecd7b7a8cd71d948
krzys-h <krzys_h@interia.pl>
2020-12-29 17:38:27 +0100

    Add populate_with_test_data test

---
+++ wwwapp/tests/test_populate_with_test_data.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.test import TestCase
+from django.test.utils import override_settings
+
+from wwwapp.models import UserProfile, Workshop
+
+
+@override_settings(DEBUG=True)
+class PopulateWithTestData(TestCase):
+    def test_populate_command(self):
+        args = []
+        opts = {}
+        call_command('populate_with_test_data', *args, **opts)
+
+        self.assertEquals(User.objects.count(), 51)
+        self.assertEquals(UserProfile.objects.count(), 51)
+        self.assertEquals(Workshop.objects.count(), 5)
