ebb44d1a1f1a78489360ac02045bdea0e70ec156
Artur Puzio <contact@puzio.waw.pl>
2025-06-29 10:29:40 +0200

    Python3

--- make_plan.py
+++ make_plan.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 # flake8: noqa
 import random
 from copy import deepcopy
@@ -8,8 +8,8 @@ import time
 from datetime import datetime
 
 if len(sys.argv) not in [2, 3]:
-    print "Usage {file} data.json".format(file=sys.argv[0])
-    print "Usage {file} data.json [[1, 2, 3], [4, 5, 6]]".format(file=sys.argv[0])
+    print("Usage {file} data.json".format(file=sys.argv[0]))
+    print("Usage {file} data.json [[1, 2, 3], [4, 5, 6]]".format(file=sys.argv[0]))
     sys.exit(1)
 
 # Reads data in format of dataForPlan.
@@ -57,16 +57,16 @@ class Plan(object):
     def __init__(self, tab=None):
         self.workshops = dict()
         if tab is None:
-            self.blocks = [set() for i in xrange(6)]
+            self.blocks = [set() for i in range(6)]
         else:
-            self.blocks = [set() for i in xrange(6)]
-            for i in xrange(6):
+            self.blocks = [set() for i in range(6)]
+            for i in range(6):
                 for wid in tab[i]:
                     self.add(i, wid)
     
     def tab(self):
         tab = []
-        for i in xrange(6):
+        for i in range(6):
             tab.append([])
             for wid in self.blocks[i]:
                 tab[i].append(wid)
@@ -112,32 +112,32 @@ class Plan(object):
     @staticmethod
     def make_random_plan():
         plan = Plan()
-        for wid in workshops.keys():
+        for wid in workshops:
             plan.add(random.randint(0, 5), wid)
         return plan
     
     def describe(self):
-        for uid in users.keys():
+        for uid in users:
             printed_user_already = False
             for block in users[uid]['blocks']:
                 wids_on_block_for_user = [wid for wid in users[uid]['part'] if wid in self.blocks[block]]
                 if len(wids_on_block_for_user) > 1:
                     if printed_user_already is False:
-                        print " *", users[uid]['name'], " registered for", len(users[uid]['part']), "workshops"
+                        print(" *", users[uid]['name'], " registered for", len(users[uid]['part']), "workshops")
                         printed_user_already = True
-                    print "  ", len(wids_on_block_for_user), "collisions:", [workshops[wid]['name'] for wid in wids_on_block_for_user]
+                    print("  ", len(wids_on_block_for_user), "collisions:", [workshops[wid]['name'] for wid in wids_on_block_for_user])
                     
         
         collision_sum = 0
         collision_user_sum = 0
-        for block in xrange(6):
-            print "BLOCK", block
+        for block in range(6):
+            print("BLOCK", block)
             for wid in self.blocks[block]:
                 participants_willing_to = 0
                 participants_today = 0
                 collisions = 0
                 collision_users = 0
-                for uid in users.keys():
+                for uid in users:
                     if wid in users[uid]['part']:
                         participants_willing_to += 1
                         if block in users[uid]['blocks']:
@@ -153,11 +153,11 @@ class Plan(object):
                                         collided = True
                                     collisions += 1
                                     collision_sum += 1
-                print " *", wid, workshops[wid]['name'], "-", [users[lid]['name'] for lid in workshops[wid]['lecturers']][0]
-                print "   participants today/willing:", participants_today, "/", participants_willing_to
-                print "   collisions / user collisions:", collisions, "/", collision_users
-            print "-------"
-        print "colisions total = {sum}, colision users total = {users}".format(sum=collision_sum, users=collision_user_sum)
+                print(" *", wid, workshops[wid]['name'], "-", [users[lid]['name'] for lid in workshops[wid]['lecturers']][0])
+                print("   participants today/willing:", participants_today, "/", participants_willing_to)
+                print("   collisions / user collisions:", collisions, "/", collision_users)
+            print("-------")
+        print("colisions total = {sum}, colision users total = {users}".format(sum=collision_sum, users=collision_user_sum))
 
     def evaluate(self, verbose=False):
         all_wids = set()
@@ -177,16 +177,16 @@ class Plan(object):
                 all_lecturer_uids.add(lec_uid)
                 if self.workshops[wid] not in users[lec_uid]['blocks']:
                     if verbose:
-                        print "COLLISION OF LECTURER"
-                        print "\tlec_uid={uid} wid={wid}".format(uid=lec_uid, wid=wid)
+                        print("COLLISION OF LECTURER")
+                        print("\tlec_uid={uid} wid={wid}".format(uid=lec_uid, wid=wid))
                     points -= 10**6
 
         for wid in all_wids:
             for disallowed_block in workshops[wid].get('disallowed_blocks', []):
                 if wid in self.blocks[disallowed_block]:
                     if verbose:
-                        print "DISALLOWED BLOCK"
-                        print "\twid={wid} block={block}".format(wid=wid, block=disallowed_block), workshops[wid]['name']
+                        print("DISALLOWED BLOCK")
+                        print("\twid={wid} block={block}".format(wid=wid, block=disallowed_block), workshops[wid]['name'])
                     points -= points_not_allowed
 
         for block in self.blocks:
@@ -195,49 +195,49 @@ class Plan(object):
                 for lec_uid in workshops[wid]['lecturers']:
                     if lec_uid in lectures_in_block:
                         if verbose:
-                            print "LECTURER IN MORE THAN ONE WORKSHOP"
-                            print "\tuid={uid} wid={wid}".format(uid=lec_uid, wid=wid)
+                            print("LECTURER IN MORE THAN ONE WORKSHOP")
+                            print("\tuid={uid} wid={wid}".format(uid=lec_uid, wid=wid))
                         points -= 10**6
                     lectures_in_block.add(lec_uid)
 
         for block in self.blocks:
             if abs(workshops_per_block - len(block)) > 0.9:
                 if verbose:
-                    print "WRONG NUMBER OF WORKSHOPS IN BLOCK {block}".format(block=block)
+                    print("WRONG NUMBER OF WORKSHOPS IN BLOCK {block}".format(block=block))
                 points -= abs(workshops_per_block - len(block)) * points_wrong_workshops_per_block
 
 
         col_counter = {wid:0 for wid in all_wids}
-        for uid in (uid for uid in users.keys() if uid not in all_lecturer_uids):
+        for uid in (uid for uid in users if uid not in all_lecturer_uids):
             user_blocks = dict()
             for wid in users[uid]['part']:
                 if self.workshops[wid] in users[uid]['blocks']:
                     if self.workshops[wid] not in user_blocks:
                         user_blocks[self.workshops[wid]] = []
                     user_blocks[self.workshops[wid]].append(wid)
-            for block in user_blocks.keys():
+            for block in user_blocks:
                 if len(user_blocks[block]) > 1:
                     for wid in user_blocks[block]:
                         col_counter[wid] += 1
                     
             empty_blocks = min(len(users[uid]['blocks']), len(users[uid]['part'])) - len(user_blocks)
             assert empty_blocks >= 0
-            #print empty_blocks, users[uid]['name']
+            #print(empty_blocks, users[uid]['name'])
             points -= empty_blocks**empty_blocks if empty_blocks>0 else 0
             if empty_blocks > 0:
                 if verbose:
-                    print empty_blocks, "EMPTY BLOCKS for", users[uid]['name']
-                    print "\tuid={uid}".format(uid=uid)
+                    print(empty_blocks, "EMPTY BLOCKS for", users[uid]['name'])
+                    print("\tuid={uid}".format(uid=uid))
             #points += len(user_blocks)
         
-        for wid in col_counter.keys():
+        for wid in col_counter:
             points_col -= col_counter[wid]**2
         
         return points * 5 + points_col
 
 def improve(plan, points):
     plan2 = plan.copy()
-    for _ in xrange(random.randint(1, 5)):
+    for _ in range(random.randint(1, 5)):
         if random.randint(0, 1) == 0:
             plan2.mutate()
         else:
@@ -251,7 +251,7 @@ def improve(plan, points):
 
 def generate_plan():
 
-    pnp = [Plan.make_random_plan() for _ in xrange(1000)]
+    pnp = [Plan.make_random_plan() for _ in range(1000)]
     pnp = [(plan, plan.evaluate()) for plan in pnp]
 
     BEST = pnp[0][1]
@@ -259,20 +259,20 @@ def generate_plan():
 
     try:
         while True:
-            for i in xrange(len(pnp)):
+            for i in range(len(pnp)):
                 pnp[i] = improve(pnp[i][0], pnp[i][1])
                 if pnp[i][1] > BEST:
                     BEST = pnp[i][1]
                 if verbose:
-                    print BEST, pnp[i][1]
+                    print(BEST, pnp[i][1])
             if last_print_time < time.time() - 1.0:
-                print BEST
+                print(BEST)
                 last_print_time = time.time()
             
     except KeyboardInterrupt:
-        print "ABORTED"
+        print("ABORTED")
 
-    for i in xrange(len(pnp)):
+    for i in range(len(pnp)):
         if pnp[i][1] == BEST:
             return pnp[i][0]
 
@@ -280,9 +280,9 @@ def generate_plan():
 def print_plan(plan):
     pts = plan.evaluate(True)
     plan.describe()
-    print "points:", pts
-    print "TAB for later use:"
-    print plan.tab()
+    print("points:", pts)
+    print("TAB for later use:")
+    print(plan.tab())
 
 if len(sys.argv) == 2:
     plan = generate_plan()
@@ -293,5 +293,5 @@ elif len(sys.argv) == 3:
     plan = Plan(plan_tab)
     print_plan(plan)
 else:
-    print "Invalid number of arguments"
+    print("Invalid number of arguments")
 
